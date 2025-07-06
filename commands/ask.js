a76c443ecb5b3c6cd284b0f22891d10e2de788e3
petrz <nhuthaotieu@gmail.com>
2025-07-07 04:07:37 +0700

    Update ask.js

--- commands/ask.js
+++ commands/ask.js
@@ -48,7 +48,7 @@ module.exports = {
      */
     async execute(interaction, deferred){
         let attachment = interaction.options.getAttachment("file");
-        let prompt = `[The current user sending the following is ${interaction.user.displayName}, mentionable with <@!${interaction.user.id}>]: ` + interaction.options.getString("question");
+        let prompt = `[The current user sending the following is ${interaction.user.displayName} with the ID ${interaction.user.id}, mentionable with <@!${interaction.user.id}>. The current date and time is ${new Date().toString()}.]: ` + interaction.options.getString("question");
         let contents = prompt;
         if(attachment){
             let attachmentData = await getAttachment(attachment);
@@ -60,7 +60,7 @@ module.exports = {
                     },
                 },
                 {text: prompt}
-            ]
+            ];
         }
         const response = await interaction.client.ai.models.generateContent({
             model: interaction.options.getString("model") ?? "gemini-2.5-flash",
