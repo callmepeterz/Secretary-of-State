fd2ea2e184d3ecb52f8d8626284844d8f1265ead
petrz <nhuthaotieu@gmail.com>
2025-07-25 23:58:50 +0700

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -17,7 +17,7 @@ module.exports = {
         let color = interaction.guild?.me?.displayHexColor || process.env.DEFAULT_COLOR;
         let embed = new EmbedBuilder().setColor(color);
         let msg = await interaction.channel?.send("Pinging...").catch(()=>{});
-        if(!msg) return interaction?.reply({embeds: [embed.setDescription("Could not get latency!")], flags: MessageFlags});
+        if(!msg) return interaction?.reply({embeds: [embed.setDescription("Could not get latency!")], flags: MessageFlags.Ephemeral});
         let msgTimestamp = msg?.createdTimestamp;
         msg.delete().catch(()=>{});
         embed.setDescription(`**Command latency: ** ${msgTimestamp - interaction.createdTimestamp} ms\n**API latency: **${interaction.client.ws.ping === -1 ? "Not available" : `${interaction.client.ws.ping} ms`}`);
