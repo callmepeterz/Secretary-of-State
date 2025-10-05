e2e0cb9700801e86af546553377fa0f3ac07c2c8
petrz <nhuthaotieu@gmail.com>
2025-10-05 15:41:30 +0700

    Update mem.js

--- commands/mem.js
+++ commands/mem.js
@@ -33,3 +33,10 @@ module.exports = {
         interaction?.reply({embeds: [embed]});
     },
 };
+
+function human(bytes) {
+  const units = ['B','KB','MB','GB','TB'];
+  let i = 0;
+  while (bytes >= 1024 && i < units.length-1) { bytes /= 1024; i++; }
+  return `${bytes.toFixed(2)} ${units[i]}`;
+}
