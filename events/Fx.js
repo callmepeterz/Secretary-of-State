7e77390b80808745d04eb3824664fa36e3c0262d
petrz <nhuthaotieu@gmail.com>
2026-08-30 17:24:10 +0700

    Add Facebook links to Fx URL rewriting
    Extend the message URL replacement handler in `events/Fx.js` to detect `facebook.com` links and rewrite them to `facebed.com`, alongside existing Twitter/X and Instagram handling.

--- events/Fx.js
+++ events/Fx.js
@@ -2,6 +2,8 @@ const { Events, Message } = require('discord.js');
 const spoilerRegex = /\|\|([^|]+)\|\|/;
 const twitterRegex = /(?<prefix>https?:\/\/(?:www\.)?)(?<domain>twitter\.com|x\.com)(?<suffix>(?:\/\S*)*)/gmi;
 const instagramRegex = /(?<prefix>https?:\/\/(?:www\.)?)(?<domain>instagram.com)(?<suffix>(?:\/\S*)*)/gmi;
+const facebookRegex = /(?<prefix>https?:\/\/(?:www\.)?)(?<domain>facebook.com)(?<suffix>(?:\/\S*)*)/gmi;
+
 const fxDomains = [
     "fxtwitter.com",
     "girlcockx.com",
@@ -21,6 +23,7 @@ module.exports = {
         let urlList = "";
         message.content?.match(twitterRegex)?.forEach(v => urlList += v.replace(twitterRegex, `$<prefix>${fxDomains[Math.floor(Math.random() * fxDomains.length)]}$<suffix>`) + "\n");
         message.content?.match(instagramRegex)?.forEach(v => urlList += v.replace(instagramRegex, `$<prefix>oginstagram.com$<suffix>`) + "\n");
+        message.content?.match(facebookRegex)?.forEach(v => urlList += v.replace(facebookRegex, `$<prefix>facebed.com$<suffix>`) + "\n");
         if(!urlList.length) return;
         if(spoilerRegex.test(message.content)) urlList = `||${urlList}||`;
         message.suppressEmbeds().catch(() => {});
