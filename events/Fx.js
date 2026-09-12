f49d5de6f87ab5e453c7a7124e56f196d3796ad0
petrz <nhuthaotieu@gmail.com>
2026-09-12 22:07:32 +0700

    Update Fx.js

--- events/Fx.js
+++ events/Fx.js
@@ -12,7 +12,7 @@ const fxList = [
     },
     {
         regex: /(?<prefix>https?:\/\/)(?:www\.)?(?<domain>facebook\.com)(?<suffix>(?:\/[^\s\|]*)*)/gmi,
-        domains: ["facebed.seria.moe"]
+        domains: ["facebed.com"]
     },
     {
         regex: /(?<prefix>https?:\/\/(?:www\.)?(?:vt\.)?)(?<domain>tiktok\.com)(?<suffix>(?:\/[^\s\|]*)*)/gmi,
