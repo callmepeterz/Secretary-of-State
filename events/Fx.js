e43d601dd57c2fcda11709f7e398710bc59ab290
petrz <nhuthaotieu@gmail.com>
2026-09-14 23:00:56 +0700

    Extract FX link rules to assets module
    Moves the hardcoded `fxList` regex/domain mapping out of `events/Fx.js` into a new `assets/fxList.js` file and imports it from there. This keeps the event handler cleaner and makes FX domain rules easier to maintain or reuse.

--- events/Fx.js
+++ events/Fx.js
@@ -1,32 +1,7 @@
 const { Events, Message } = require('discord.js');
 const spoilerRegex = /\|\|([^\|]+)\|\|/;
 
-const fxList = [
-    {
-        regex: /(?<prefix>https?:\/\/)(?:www\.)?(?<domain>twitter\.com|x\.com)(?<suffix>(?:\/[^\s\|]*)*)/gmi,
-        domains: ["fxtwitter.com", "girlcockx.com", "hotyurisex.com", "yaoisex.com", "boypussyx.com"]
-    },
-    {
-        regex: /(?<prefix>https?:\/\/)(?:www\.)?(?<domain>instagram\.com)(?<suffix>(?:\/[^\s\|]*)*)/gmi,
-        domains: ["oginstagram.com"]
-    },
-    {
-        regex: /(?<prefix>https?:\/\/)(?:www\.)?(?<domain>facebook\.com)(?<suffix>(?:\/[^\s\|]*)*)/gmi,
-        domains: ["facebed.seria.moe"]
-    },
-    {
-        regex: /(?<prefix>https?:\/\/(?:www\.)?(?:vt\.)?)(?<domain>tiktok\.com)(?<suffix>(?:\/[^\s\|]*)*)/gmi,
-        domains: ["tnktok.com"]
-    },
-    {
-        regex: /(?<prefix>https?:\/\/(?:www\.)?)(?<domain>youtube\.com)(?<suffix>\/shorts(?:\/[^\s\|]*)*)/gmi,
-        domains: ["koutube.com"]
-    },
-    {
-        regex: /(?<prefix>https?:\/\/)(?:www\.)?(?<domain>threads\.com)(?<suffix>(?:\/[^\s\|]*)*)/gmi,
-        domains: ["fixthreads.seria.moe"]
-    },
-];
+const fxList = require("../assets/fxList.js");
 
 
 module.exports = {
