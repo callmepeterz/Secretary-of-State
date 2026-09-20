18d7c853f8fcb90324a5e13892e0812031bf7294
petrz <nhuthaotieu@gmail.com>
2026-09-21 02:00:05 +0700

    Update reload.js

--- commands/reload.js
+++ commands/reload.js
@@ -128,6 +128,8 @@ module.exports = {
             const filePath = path.join(eventsPath, file);
             delete require.cache[require.resolve(filePath)];
             const event = require(filePath);
+
+            interaction.client.removeListener(event.name);
             if (event.once) {
                 interaction.client.once(event.name, (...args) => event.execute(...args));
             }
